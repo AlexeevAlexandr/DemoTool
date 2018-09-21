60faefc09ea7754bf502c97e2b161cb93c512854
Alexandr Alexeev <fart82@ukr.net>
2018-09-22 00:34:48 +0300

    did work with adding 100K rows

--- src/main/java/inputData/Input100KRows.java
+++ src/main/java/inputData/Input100KRows.java
@@ -10,17 +10,10 @@ class Input100KRows {
     String add100KRows() {
 
         String a = random_C_or_D();
-        String b = (a.equals("C")) ? " " + randomNumber() + "," : ",";
+        String b = (a.equals("C")) ? " " + randomNumber2() + "," : "-" + date() + ",";
 
         return a + " " + randomNumber() + " " + randomNumber() + " " + random_P_or_N() + " " + date() + b;
 
-//                "C 1.1 8.15.1 P 15.10.2012 83 " +
-//                "C 1 10.1 P 01.12.2012 65 " +
-//                "C 1.1 5.5.1 P 01.11.2012 117 " +
-//                "D 1.1 8 P 01.01.2012-01.12.2012 " +
-//                "C 3 10.2 N 02.10.2012 100 " +
-//                "D 1 * P 8.10.2012-20.11.2012 " +
-//                "D 3 10 P 01.12.2012";
     }
 
     private String random_C_or_D() {
@@ -33,6 +26,10 @@ class Input100KRows {
 
     private String randomNumber(){
         return String.valueOf((int) ((Math.random()+1) * 3));
+    }
+
+    private String randomNumber2(){
+        return String.valueOf((int) (Math.random() * 200));
     }
 
     private String date(){
--- src/main/java/services/Parsing.java
+++ src/main/java/services/Parsing.java
@@ -12,7 +12,7 @@ import java.util.Date;
 public class Parsing {
 
     private Date parseDate(String string) throws ParseException {
-        DateFormat format = new SimpleDateFormat("d.MMMM.yyyy");
+        DateFormat format = new SimpleDateFormat("dd.MM.yyyy");
         return format.parse(string);
     }
 
@@ -27,12 +27,15 @@ public class Parsing {
         Parsing parsing = new Parsing();
         InputData inputData = new InputData();
         ArrayList<Object> input = new ArrayList<>();
-        String in = "4," +
-                "C 1.1 5.5.1 P 01.11.2012 117," +
-                "D 1.1 8 P 01.01.2012-01.12.2012," +
-                "C 1.1 5.5.1 P 01.11.2012 118," +
-                "D 1.2 8 P 01.01.2012-01.12.2012";
-//        String in = inputData.input100KRows();
+//        String in = "7," +
+//                "C 1.1 8.15.1 P 15.10.2012 83," +
+//                "C 1 10.1 P 01.12.2012 65," +
+//                "C 1.1 5.5.1 P 01.11.2012 117," +
+//                "D 1.1 8 P 01.01.2012-01.12.2012," +
+//                "C 3 10.2 N 02.10.2012 100," +
+//                "D 1 * P 8.10.2012-20.11.2012," +
+//                "D 3 10 P 01.12.2012";
+        String in = inputData.input100KRows();
         Collections.addAll(input, in.split(","));
 
         int capacity = Integer.parseInt(input.get(0).toString());
@@ -44,45 +47,61 @@ public class Parsing {
             ArrayList<Object> input2 = new ArrayList<>();
             in2 = input.get(i).toString();
             Collections.addAll(input2, in2.split(" "));
-            if (input2.get(0).toString().equals("C")){
+            if (input2.get(0).toString().equals("C")) {
                 Collections.addAll(inputC, input2);
             } else {
                 Collections.addAll(inputD, input2);
             }
         }
-
-        System.out.println("Objects C:");
-        for (Object object : inputC){
-        System.out.println(object);
-        }
-
-        System.out.println("Objects D:");
-        for (Object object : inputD){
-        System.out.println(object);
-        }
-
+        String inD;
         String inC;
-        String inC2;
-        for (int i=0; i<inputD.size(); i++) {
+        String inDate;
+        for (Object anInputD : inputD) {
             ArrayList<Object> parserD = new ArrayList<>();
-            inC = inputC.get(i).toString().replaceAll("[\\[\\] ]", "");
-            Collections.addAll(parserD, inC.split(","));
+            inD = anInputD.toString().replaceAll("[\\[\\] ]", "");
+            Collections.addAll(parserD, inD.split(","));
 
+            ArrayList<Object> listParseDateD = new ArrayList<>();
+            inDate = parserD.get(4).toString().replaceAll("[\\[\\] ]", "");
+            Collections.addAll(listParseDateD, inDate.split("-"));
+            if (listParseDateD.size() == 1) {
+                listParseDateD.add(parserD.get(4).toString().replaceAll("[\\[\\] ]", ""));
+            }
 
-            System.out.println((parserD.get(1).toString().substring(0,1)));
-
-            for (int ii = 0; ii < inputC.size(); ii++) {
+            int count = 0;
+            int result = 0;
+            for (Object anInputC : inputC) {
                 ArrayList<Object> parserC = new ArrayList<>();
-                inC2 = inputD.get(ii).toString().replaceAll("[\\[\\] ]", "");
-                Collections.addAll(parserC, inC2.split(","));
+                inC = anInputC.toString().replaceAll("[\\[\\] ]", "");
+                Collections.addAll(parserC, inC.split(","));
 
-                if (((parserD.get(1).toString().substring(0,1)).equals(parserC.get(1).toString().substring(0,1))) &&
-                        ((parserD.get(2).toString().substring(0,1)).equals(parserC.get(2).toString().substring(0,1))) &&
-                        parsing.between(parsing.parseDate(parserC.get(4).toString()), )
-                {
-                    System.out.println("ok");
+                String s = parserD.get(2).toString();
+                if (!s.equals("*")) {
+                    if (((parserD.get(1).toString().substring(0, 1)).equals(parserC.get(1).toString().substring(0, 1))) &&
+                            ((parserD.get(2).toString().substring(0, 1)).equals(parserC.get(2).toString().substring(0, 1))) &&
+                            (parsing.between(parsing.parseDate(parserC.get(4).toString()),
+                                    parsing.parseDate(listParseDateD.get(0).toString()),
+                                    parsing.parseDate(listParseDateD.get(1).toString())))) {
+                        count++;
+                        result += Integer.parseInt(parserC.get(5).toString());
+                    }
+                } else {
+                    if (((parserD.get(1).toString().substring(0, 1)).equals(parserC.get(1).toString().substring(0, 1))) &&
+                            (parsing.between(parsing.parseDate(parserC.get(4).toString()),
+                                    parsing.parseDate(listParseDateD.get(0).toString()),
+                                    parsing.parseDate(listParseDateD.get(1).toString())))) {
+                        count++;
+                        result += Integer.parseInt(parserC.get(5).toString());
+                    }
                 }
+            }
+            if (result > 0) {
+                System.out.print(result / count + " ");
+            } else {
+                System.out.print("- ");
             }
         }
     }
 }
+
+
